01f208d6b7fb2a5c11c6c08d35385a8b8cb6b9ce
mdadilfaiz48 <mdadilfaiz48@gmail.com>
2026-06-30 11:59:56 +0530

    first commit in feature/displayTag branch

--- src/main/java/com/adil/tags/DisplayTag.java
+++ src/main/java/com/adil/tags/DisplayTag.java
@@ -1,8 +1,43 @@
 package com.adil.tags;
 
+import jakarta.servlet.jsp.JspException;
+import jakarta.servlet.jsp.JspWriter;
 import jakarta.servlet.jsp.tagext.TagSupport;
 
 public class DisplayTag extends TagSupport {
 	private static final long serialVersionUID = 1L;
+	private int size =20;
+	private String font;
 
-}
+	public void setSize(int size) {
+		this.size = size;
+	}
+
+	public void setFont(String font) {
+		this.font = font;
+	}
+
+
+  
+	public int doStartTag()throws JspException {
+		  // get out object
+		 JspWriter jw = pageContext.getOut();
+		   try {
+			     jw.println("<span style='font-family:"+font+";font-size:"+size+"+px;'>");
+		   }catch(Exception e){
+			    e.printStackTrace();
+		   }
+		return EVAL_BODY_INCLUDE;
+	}//doStartTag()
+	
+	public int doEndTag()throws JspException {
+		  // get out object
+		 JspWriter jw = pageContext.getOut();
+		   try {
+			     jw.println("</span>");
+		   }catch(Exception e){
+			    e.printStackTrace();
+		   }
+		 return EVAL_PAGE;
+	}// doEndTag()
+}//End class
--- src/main/webapp/test_tag.jsp
+++ src/main/webapp/test_tag.jsp
@@ -2,3 +2,12 @@
 <nit:printLabel/>:: <nit:prime n="200"/>
 <br>
 <nit:printLabel/> :: <nit:prime/>
+<br>
+<br>
+
+<nit:display font="arial" size="200">How are you?</nit:display>
+
+<br>
+<br>
+
+<nit:display font="Webdings" size="60">Fine, Thanks You</nit:display>
